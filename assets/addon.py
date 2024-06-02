63400f7f4cff0b4ef6755563419aa6f7291b7905
lostact <lostact@gmail.com>
2024-06-03 01:48:00 +0330

    new method implemented
    we are now using mitmproxy + dns server

---
+++ assets/addon.py
@@ -0,0 +1,31 @@
+from mitmproxy import ctx
+from time import time
+import os 
+
+class SteamDL:
+    def load(self, loader):
+        loader.add_option(name="token",typespec=str,default="",help="User Token")
+        mode_string = ctx.options.mode[0]
+        domain_start = mode_string.find("//") + 2
+        self.cache_domain = mode_string[domain_start:].split("@")[0]
+        self.last_update_time = 0
+        self.rx_bytes = 0
+        if os.path.isfile("rx.txt"):
+            with open("rx.txt", 'r') as rx_file:
+                self.rx_bytes = int(rx_file.read().strip())
+        
+    def requestheaders(self, flow):
+        if flow.request.method == "GET":
+            flow.request.headers["Real-Host"] = flow.request.host_header
+            flow.request.headers["Host"] = self.cache_domain    
+            flow.request.headers["Auth-Token"] = ctx.options.token
+
+    def responseheaders(self,flow):
+        self.rx_bytes += int(flow.response.headers.get("Content-Length", 0))
+        current_time = time()
+        if current_time - self.last_update_time > 2:
+            self.last_update_time = current_time
+            with open("rx.txt", "w") as rx_file:
+                rx_file.write(str(self.rx_bytes))
+
+addons = [SteamDL()]
--- main.py
+++ main.py
@@ -1,106 +1,183 @@
-import webview, sys, os, requests, json, subprocess, threading
-
-from random import choices
-from string import ascii_lowercase, digits
+import webview, sys, os, requests, json, subprocess, threading, socket, wmi, signal
 
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
-    except Exception:
+    except:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
 
-VERSION = "0.4.4"
+def get_all_networks_and_dns_servers():
+    wmi_service = wmi.WMI()
+
+    networks_and_servers = {adaptor.Description: adaptor.DNSServerSearchOrder
+                            for adaptor in wmi_service.Win32_NetworkAdapterConfiguration(IPEnabled=True)
+                            if "Virtual" not in adaptor.Description}
+
+    return networks_and_servers
+
+
+def set_dns_of_network(action, network_name, dns_servers=None):
+    wmi_service = wmi.WMI()
+    network = wmi_service.Win32_NetworkAdapterConfiguration(IPEnabled=True, Description=network_name)[0]
+    network.SetDNSServerSearchOrder(dns_servers) if action == "change" else network.SetDNSServerSearchOrder()
+
+VERSION = "1.0.0"
 WINDOW_TITLE = "SteamDL v{}".format(VERSION)
 
-SUB_URL = "https://dl.gamegk.ir/sub/"
+CACHE_DOMAIN = "dl.steamdl.ir"
+API_DOMAIN = "api.steamdl.ir"
 
-SINGBOX_PATH = resource_path('assets/sing-box.exe')
+MITM_PATH = resource_path('assets/mitmdump.exe')
+MITM_ADDON_PATH = resource_path('assets/addon.py')
+MITM_RX_PATH = resource_path('rx.txt')
+
 INDEX_PATH = resource_path('assets/web/index.html')
 FORM_PATH = resource_path('assets/web/form.html')
-CONFIG_PATH = resource_path('assets/sing-box.json')
+
+SEARCH_IP_BYTES = socket.inet_aton("127.0.0.1")
 
 class Api:
     def __init__(self):
         self._window = None
         self._token = None
-        self._singbox_process = None
+        self._user_data = None
+
+        self._cache_ip = None
+        self._local_ip = None
+        self._local_ip_bytes = None
+
+        self._mitm_process = None
+        self._dns_thread = None
+        self._dns_backup = None
+
+    def get_default_interface_ip(self):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.connect((self._cache_ip, 53))
+                ip_address = s.getsockname()[0]
+            return ip_address
+        except Exception as e:
+            print(f"Error obtaining default interface IP: {e}")
+            return None
+
+    def handle_dns_client(self, data, client_address, dns_socket):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as upstream_socket:
+            upstream_socket.sendto(data, (self._cache_ip, 53))
+            response_data_bytes, _ = upstream_socket.recvfrom(512)
+            response_data = bytearray(response_data_bytes)
+            start = response_data.find(SEARCH_IP_BYTES)
+            if start != -1:
+                response_data[start:start+len(SEARCH_IP_BYTES)] = self._local_ip_bytes
+            dns_socket.sendto(bytes(response_data), client_address)
+
+    def start_dns(self):
+        self._dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._dns_socket.bind((self._local_ip, 53))
+        print(f"UDP proxy listening on {self._local_ip}:53")
+        while self.check_mitm_status():
+            try:
+                data, client_address = self._dns_socket.recvfrom(512)
+                client_thread = threading.Thread(target=self.handle_dns_client, args=(data, client_address, self._dns_socket))
+                client_thread.start()
+            except:
+                pass
 
     def set_window(self, window):
         self._window = window
 
-    def set_token(self, token):
-        self._token = token
-
-    def save_interface_name(self):
-        with open(CONFIG_PATH, "r") as singbox_config_file:
-            singbox_config = json.loads(singbox_config_file.read())
-
-        interface_name = "".join(choices(ascii_lowercase, k=3)) + "".join(choices(digits, k=2))
-        singbox_config["inbounds"][0]["interface_name"] = interface_name 
-
-        with open(CONFIG_PATH, "w") as singbox_config_file:
-            singbox_config_file.write(json.dumps(singbox_config, indent=4))
-
     def submit_token(self, token):
         self._token = token
-        success = self.update_config()
+        response = requests.get(f"http://{API_DOMAIN}/get_user?token=" + self._token)
+        success = False
+        if response:
+            success = bool(response.status_code == 200)
+
         if not success:
             window.evaluate_js("document.getElementById('error').style.display = 'block';")
             return
 
+        self._user_data = json.loads(response.content)
+        self._cache_ip = response.headers['X-Server-IP']
         with open("account.txt", "w") as account_file:
             account_file.write(token)
 
-        self.update_index()
-        self._window.load_url(INDEX_PATH)
-
-    def toggle_vpn(self):
-        running = self.check_vpn_status()
+    def toggle_mitm(self):
+        running = self.check_mitm_status()
         if running:
-            self._singbox_process.terminate()
+            local_ip = ""
+            print("Stopping Service...")
+
+            # Kill MITMProxy:
+            print("Killing MITM...")
+            subprocess.call(['taskkill', '/PID', str(self._mitm_process.pid), '/T', '/F'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            # Restore system DNS servers:
+            print("Restoring system DNS servers...")
+            for network in self._dns_backup:
+                if len(self._dns_backup[network]) > 1:
+                    set_dns_of_network("change", network, self._dns_backup[network])
+                else:
+                    set_dns_of_network("clear", network)
         else:
-            self.save_interface_name()
-            self._singbox_process = subprocess.Popen((SINGBOX_PATH + ' run -c "' + CONFIG_PATH + '"'), close_fds=True, creationflags=134217728, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            result = self._singbox_process.communicate()
-            for line in result[1].decode(encoding='utf-8').strip().split('\n'):
-                print(line)
+            print("Starting Service...")
+            token = self._token
+            cache_ip = self._cache_ip
+            local_ip = self._local_ip = self.get_default_interface_ip()
+            local_ip_bytes = self._local_ip_bytes = socket.inet_aton(local_ip)
+
+            # Run MITMProxy:
+            print("Starting MITM...")
+            self._mitm_process = subprocess.Popen(f"{MITM_PATH} --mode reverse:http://{CACHE_DOMAIN}@{local_ip}:80 --mode reverse:https://{cache_ip}@{local_ip}:443 --set keep_host_header=true --set allow_hosts={CACHE_DOMAIN} -s {MITM_ADDON_PATH}  --set token={token} --set flow_detail=0 --set termlog_verbosity=debug", close_fds=True, creationflags=134217728, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            # result = self._mitm_process.communicate()
+            # for line in result[1].decode(encoding='utf-8').strip().split('\n'):
+            #     print(line)
+            # with self._mitm_process.stdout:
+            #     for line in iter(self._mitm_process.stdout.readline, b''):
+            #         print(line.decode("utf-8").strip())
 
 
+            # Run DNS reverse proxy:
+            print("Starting DNS server...")
+            if not self._dns_thread:
+                dns_thread = self._dns_thread = threading.Thread(target=self.start_dns, daemon=True)
+                dns_thread.start()
 
-    def check_vpn_status(self):
-        if self._singbox_process:
-            return self._singbox_process.poll() == None
+            # Change system DNS servers:
+            print("Changing system DNS servers...")
+            dns_backup = self._dns_backup = get_all_networks_and_dns_servers()
+            for network in dns_backup:
+                set_dns_of_network("change", network, (local_ip, "1.1.1.1"))
+        return local_ip
+
+
+    def check_mitm_status(self):
+        if self._mitm_process:
+            return self._mitm_process.poll() == None
         else:
             return False
+
+    def get_user_data(self):
+        print("User Data:", self._user_data)
+        return self._user_data
+
+    def get_rx(self):
+        if os.path.isfile(MITM_RX_PATH):
+            with open(MITM_RX_PATH, 'r') as rx_file:
+                rx = int(rx_file.read().strip())
+                return rx
+        else:
+            return 0
 
     def minimize(self):
         self._window.minimize()
 
     def close(self):
-        running = self.check_vpn_status()
-        if running:
-            self._singbox_process.terminate()
         self._window.destroy()
-        os._exit(1)
 
-    def update_index(self):
-        html = requests.get(SUB_URL + self._token, headers={'Accept': "text/html"}).text
-        html = html.replace("</head>", "<div class='version'>v{}</div>\n</head>".format(VERSION))
-        with open(INDEX_PATH, "w", encoding="utf-8") as index_file:
-            index_file.write(html)
-            print("updated index")
-
-    def update_config(self):
-        try:
-            config_json = json.loads(requests.get(SUB_URL + self._token + "/sing-box").text)
-            if config_json:
-                with open(CONFIG_PATH, "w") as singbox_config_file:
-                    singbox_config_file.write(json.dumps(config_json, indent=4))
-                    return True
-        except:
-            return False
+    def get_version(self):
+        return VERSION
 
 if __name__ == '__main__':
     api = Api()
@@ -108,13 +185,11 @@ if __name__ == '__main__':
     success = False
     if os.path.isfile("account.txt"):
         with open("account.txt", "r") as account_file:
-            token = account_file.read()
+            token = account_file.read().strip()
             if token:
-                api.set_token(token)
-                success = api.update_config()
+                api.submit_token(token)
 
-    if success:
-        api.update_index()
+    if api._user_data:
         window = webview.create_window(WINDOW_TITLE, INDEX_PATH, width=400,height=600,js_api=api, frameless=True)
         api.set_window(window) 
     else:
@@ -122,3 +197,8 @@ if __name__ == '__main__':
         api.set_window(window)
 
     webview.start()
+
+    # Quit:
+    running = api.check_mitm_status()
+    if running:
+        api.toggle_mitm()
