0603d2041d685e5c07136b5ce4be84094b6b218c
lostact <lostact@gmail.com>
2025-03-13 00:19:07 +0330

    minor changes to the msi installer
    changed install path from program files (x86) to program files
    added shortcut
    added removal of legacy nsis install if exists
    added manufacturer info

--- setup.py
+++ setup.py
@@ -12,19 +12,19 @@ build_exe_options = {
 }
 
 bdist_msi_options = {
-    "initial_target_dir": "[ProgramFilesFolder]SteamDL",
+    "initial_target_dir": "[ProgramFiles64Folder]SteamDL",
     "upgrade_code": "{6D92AF12-4EFC-3241-88B7-84B0C6959C53}",
     "install_icon": "steamdl.ico",
     "summary_data": {"author": "SteamDL.ir"},
     "all_users": True,
-    # 34: exe file with directory - 1024: deferred - 192: asynchronous without waiting
-    "data": {"CustomAction": [("LaunchApp", 34 + 3072 + 192, "TARGETDIR", "[TARGETDIR]steamdl.exe")], "InstallExecuteSequence": [("LaunchApp", "NOT REMOVE", 6500)]}
+    # 34: exe file with directory and path - 1024: deferred - 192: asynchronous without waiting - 50: exe file with path - 64: ignore exit code
+    "data": {"Directory": [("ProgramMenuFolder", "TARGETDIR", "."), ("SteamDL", "ProgramMenuFolder", "SteamDL~1|SteamDL")], "CustomAction": [("LaunchApp", 34 + 3072 + 192, "TARGETDIR", "[TARGETDIR]steamdl.exe"), ("RemoveLegacy", 50 + 64, "LegacyUninstallerPath", "/S")],"Property":[("LegacyUninstallerPath", "C:\\Program Files (x86)\\SteamDL\\uninstall.exe")], "InstallExecuteSequence": [("RemoveLegacy", "NOT REMOVE", 1300), ("LaunchApp", "NOT REMOVE", 6500)]}
 }
-
 setup(
     name = "SteamDL",
     version = VERSION,
+    author = "SteamDL.ir",
     url = "https://steamdl.ir",
     options = {"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
-    executables = [Executable("main.py", target_name="SteamDL", icon="steamdl.ico", uac_admin=True, shortcut_name="SteamDL", base="gui")]
+    executables = [Executable("main.py", target_name="SteamDL", icon="steamdl.ico", uac_admin=True, shortcut_name="SteamDL",shortcut_dir="SteamDL", base="gui")]
 )
